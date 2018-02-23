da820957f793aabf4b871b237f8b5ab0e7489f43
Anthony Kehn <anthony@kehn.net>
2018-02-23 12:53:41 -0700

    Final V2

--- server.js
+++ server.js
@@ -24,4 +24,6 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-app.listen(port);
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+})
