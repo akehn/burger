1c64659b0494628d37c71bd5371fa1cfebcae009
Anthony Kehn <anthony@kehn.net>
2018-02-23 12:23:26 -0700

    Final V1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ultimate Sriracha Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoky Black Bean Burger', false);
